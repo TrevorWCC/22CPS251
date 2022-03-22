b720436278dd71b6ddee8def85af2c9466464f22
tabean <tabean@wccnet.edu>
2022-03-22 00:04:37 -0400

    Backwards

--- LifeCycleAwareTB/app/src/main/java/com/example/lifecycleawaretb/LifeObserver.kt
+++ LifeCycleAwareTB/app/src/main/java/com/example/lifecycleawaretb/LifeObserver.kt
@@ -9,41 +9,42 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ViewModel
 import com.example.lifecycleawaretb.ui.main.MainViewModel
+import java.time.LocalDateTime
 
 class LifeObserver: LifecycleObserver {
     var output = ""
-    private val LOG_TAG = "LifeObserver"
+    private val LOG_TAG = "MainViewModel"
     private var viewModel = MainViewModel
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
         Log.i(LOG_TAG, "onResume")
-        viewModel.setOut("onResume was fired on this date")
+        viewModel.setOut("onResume was fired on this date " + LocalDateTime.now()+ "\n*******\n")
     }
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
         Log.i(LOG_TAG, "onPause")
-        viewModel.setOut("onPause was fired on this date")
+        viewModel.setOut("onPause was fired on this date " + LocalDateTime.now()+ "\n*******\n")
     }
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate() {
         Log.i(LOG_TAG, "onCreate")
-        viewModel.setOut("onCreate was fired on this date")
+        viewModel.setOut("onCreate was fired on this date " + LocalDateTime.now()+ "\n*******\n")
     }
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStart() {
         Log.i(LOG_TAG, "onStart")
-        viewModel.setOut("onStart was fired on this date")
+        viewModel.setOut("onStart was fired on this date "+ LocalDateTime.now()+ "\n*******\n")
     }
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStop() {
         Log.i(LOG_TAG, "onStop")
-        viewModel.setOut("onStop was fired on this date")
+        viewModel.setOut("onStop was fired on this date "+ LocalDateTime.now()+ "\n*******\n")
     }
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy() {
         Log.i(LOG_TAG, "onDestroy")
-        viewModel.setOut("onDestroy was fired on this date")
+        viewModel.setOut("onDestroy was fired on this date "+ LocalDateTime.now()+ "\n*******\n")
     }
 
 }
--- LifeCycleAwareTB/app/src/main/java/com/example/lifecycleawaretb/ui/main/MainViewModel.kt
+++ LifeCycleAwareTB/app/src/main/java/com/example/lifecycleawaretb/ui/main/MainViewModel.kt
@@ -1,23 +1,29 @@
 package com.example.lifecycleawaretb.ui.main
 
+import android.util.Log
+import androidx.core.content.PackageManagerCompat.LOG_TAG
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
 
 class MainViewModel : ViewModel() {
     private var String = ""
     private var output: MutableLiveData<String> = MutableLiveData()
+    private val LOG_TAG = "MainViewModel"
 
     companion object {
+        private val LOG_TAG = "MainViewModel"
         private var String = ""
         private var output: MutableLiveData<String> = MutableLiveData()
         fun setOut(output: String) {
             String += "${output}"
             this.output.value = String
+            Log.i(LOG_TAG, this.output.value.toString())
         }
     }
     fun setOut(output: String) {
         String += "${output}"
             this.output.value = String
+        Log.i(LOG_TAG, this.output.value.toString())
     }
 
     fun getResult(): MutableLiveData<String> {
